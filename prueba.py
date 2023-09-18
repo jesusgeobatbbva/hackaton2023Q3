7602995870f5ffe36edd76321b9793d608a00d38
Astrid Vergara <65421899+AstridVergara@users.noreply.github.com>
2023-09-18 16:12:54 -0600

    Update prueba.py
    Cambio de lenguaje

--- prueba.py
+++ prueba.py
@@ -1,49 +1,9 @@
-def depositos():
-    cursor = conexion.cursor()
-    id = input("\nIngrese el ID para el déposito\n >ID: " )
-    idReal=validarID(id)
-    
-    if idReal!=False{
-        monto = input("Ingrese el monto para el déposito\n >Monto: " )
-        montoReal=validarMonto(monto)
-        if montoReal!=False{
-            sqlSaldo="SELECT Saldo FROM Client WHERE ID = " + id
-            ##Ejecutar intrución SQL
-            cursor.execute(sqlID)
-            ##Obtener resultados
-            saldoActual=cursor.fetchall()[0][0]
-            ##Se regresa una lista de tuplas con los datos
-            saldoNuevo=saldoActual+monto
-            sqlSaldoNuevo="UPDATE Clien SET Saldo = " + saldoNuevo + "WHERE ID = " + id
-            ##Ejecutar intrución SQL
-            cursor.execute(sqlID)
-        }
-    }
-    
-    
-    
-    def validarMonto(monto):
-        
-        try:
-            float(monto)
-            return True
-        except:
-            print("El digito ingresado debe ser un número")
-            return False
-        
-    def validarID(id):
-        try:
-            sqlID="SELECT * FROM Client WHERE ID = " + id
-            ##Ejecutar intrución SQL
-            cursor.execute(sqlID)
-            ##Obtener resultados
-            client=cursor.fetchall()
-            ##Se regresa una lista de tuplas con los datos
-            print("Número de cliente: " + client[0][0])
-            return True
-            
-        except:
-            print("ERROR\nEl ID no existe")
-            return False
-        
-            
+def hacer_deposito():
+    cuenta_id = int(cuenta_id_entry.get())
+    monto = float(deposito_entry.get())
+    conn = sqlite3.connect('bancaBD.db')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Cuentas SET Saldo=Saldo+? WHERE idCuentas=?", (monto, cuenta_id))
+    conn.commit()
+    conn.close()
+    consultar_saldo()
