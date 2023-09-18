b721d6b6ddc82b7e26a66144c74496e856d268b2
SaulDael <123760157+SaulDael@users.noreply.github.com>
2023-09-18 15:23:42 -0600

    cambios database
    cambios conforme al front

--- banco.sql
+++ banco.sql
@@ -2,24 +2,24 @@ create database banco;
 use banco;
 
 create table Cuentas(
-ID varchar(50) primary key not null ,
+IdCuentas INT primary key not null auto_increment,
 NombreCliente varchar(100) not null,
 Saldo decimal(10, 2)
 );
 
 create table Transacciones(
-ID varchar(50) primary key not null,
-IDCuenta varchar(50) not null,
+IDTrans INT primary key not null auto_increment,
+IdCuentas int not null,
 Tipo varchar(15) not null,
 Monto decimal(10,2) not null,
-Fecha timestamp default current_timestamp,
+Fecha int,
 
-foreign key (IDCuenta) references Cuentas(ID) on delete cascade
+foreign key (IDCuentas) references Cuentas(IDCuentas) on delete cascade
 );
 
-insert into Cuentas (ID, NombreCliente, Saldo) values ('BBVA001', 'Amelia Rivera Calva', 45977.30);
-insert into Cuentas (ID, NombreCliente, Saldo) values ('BBVA002', 'Abril  Ocaña Almeda', 2112.77);
-insert into Cuentas (ID, NombreCliente, Saldo) values ('BBVA003', 'Claudia Carolina Molina Almeda', 4124.97);
-insert into Cuentas (ID, NombreCliente, Saldo) values ('BBVA004', 'Greta Arteaga Guarneros', 37907.46);
-insert into Cuentas (ID, NombreCliente, Saldo) values ('BBVA005', 'Enzo Olavarrieta Arellanes', 15210.98);
+insert into Cuentas (ID, NombreCliente, Saldo) values ('1', 'Amelia Rivera Calva', 45977.30);
+insert into Cuentas (ID, NombreCliente, Saldo) values ('2', 'Abril  Ocaña Almeda', 2112.77);
+insert into Cuentas (ID, NombreCliente, Saldo) values ('3', 'Claudia Carolina Molina Almeda', 4124.97);
+insert into Cuentas (ID, NombreCliente, Saldo) values ('4', 'Greta Arteaga Guarneros', 37907.46);
+insert into Cuentas (ID, NombreCliente, Saldo) values ('5', 'Enzo Olavarrieta Arellanes', 15210.98);
 
